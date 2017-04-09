3358ede59cdbdadb23407e1a037627927a30a4f5
amandahogan <hogan.ans@gmail.com>
2017-04-09 22:16:31 +1200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <a-scene>
+        <a-sky src='wero.jpg' rotation='0 -130 0'></a-sky>
+      </a-scene>
+    )
+  }
+}
+
+export default App
