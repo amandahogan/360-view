8a9e55ba282baa654670c93f98506a52f15016e3
amandahogan <hogan.ans@gmail.com>
2017-04-13 12:53:37 +1200

    Deploy test with commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import 'aframe'
+import 'aframe-particle-system-component'
+import {Entity, Scene} from 'aframe-react'
+
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <a-scene>
-        <a-sky src='wero.jpg' rotation='0 -240 0'></a-sky>
-      </a-scene>
+      <Scene>
+        <Entity primitive='a-sky' src='wero.jpg' rotation='0 -240 0' />
+      </Scene>
     )
   }
 }
